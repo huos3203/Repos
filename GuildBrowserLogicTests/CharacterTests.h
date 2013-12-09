62af0182d9ee93a2c5abea71f6f5be92d6466422
pengyucheng <pengyucheng@pengyuchengtekiMac-mini.local>
2013-12-09 15:19:50 +0800

    添加characterTests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by pengyucheng on 13-12-4.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
